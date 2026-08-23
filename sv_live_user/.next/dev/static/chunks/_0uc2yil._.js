d4303d9ac62ea36ca36edfedc97c3aa98b677982
vthanneeru070 <vthanneeru@linkfields.com>
2026-08-24 00:16:44 +0530

    Stop tracking accidental .next build artifacts

--- sv_live_user/.next/dev/static/chunks/_0uc2yil._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0jl0w9p._.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/node_modules_next_dist_0b_t304._.js","static/chunks/_1c2-ltq._.js","static/chunks/node_modules_next_1i591lv._.js","static/chunks/_1mn9osm._.css"],
-    source: "entry"
-});
